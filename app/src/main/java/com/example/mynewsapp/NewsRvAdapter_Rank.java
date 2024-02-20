6d2bc094ba15570377270c4adcce05d5fbaa1c9b
Do - un Yun <106233376+yundoun@users.noreply.github.com>
2024-02-20 15:59:55 +0900

    Feat Glide 이미지 로드

--- app/src/main/java/com/example/mynewsapp/NewsRvAdapter_Rank.java
+++ app/src/main/java/com/example/mynewsapp/NewsRvAdapter_Rank.java
@@ -8,7 +8,10 @@ import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
+import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.recyclerview.widget.RecyclerView;
+
+import com.bumptech.glide.Glide;
 
 import java.util.ArrayList;
 
@@ -40,22 +43,45 @@ public class NewsRvAdapter_Rank extends RecyclerView.Adapter<NewsRvAdapter_Rank.
         holder.title.setText(newsItem.getTitle());
         holder.company.setText(newsItem.getCompany());
         holder.date.setText(newsItem.getDate());
+
+        if (position < 5) {
+            // 1번부터 5번 항목까지는 이미지뷰를 보이게 합니다.
+            holder.image.setVisibility(View.VISIBLE);
+            // 제목 텍스트뷰의 제약 조건을 이미지뷰에 맞춰 조절
+
+            Glide.with(context)
+                    .load(newsItem.getUrl()) // 여기서 getUrl()은 Api_NewsItem에서 이미지 URL을 반환하는 메소드입니다.
+                    .into(holder.image); // 이미지를 로드할 ImageView 지정
+
+            ConstraintLayout.LayoutParams params = (ConstraintLayout.LayoutParams) holder.title.getLayoutParams();
+            params.endToEnd = ConstraintLayout.LayoutParams.UNSET; // 끝 제약을 해제
+            params.endToStart = R.id.iv_rank; // 끝을 이미지뷰의 시작으로 설정
+            holder.title.setLayoutParams(params); // 변경된 파라미터를 적용
+        } else {
+            // 6번 이후의 항목에서는 이미지뷰를 숨깁니다.
+            holder.image.setVisibility(View.GONE);
+            // 제목 텍스트뷰의 제약 조건을 부모 끝에 맞춰 전체 너비를 사용하도록 조절
+            ConstraintLayout.LayoutParams params = (ConstraintLayout.LayoutParams) holder.title.getLayoutParams();
+            params.endToStart = ConstraintLayout.LayoutParams.UNSET; // 시작 제약을 해제
+            params.endToEnd = R.id.item_rank; // 끝을 부모의 끝으로 설정
+            holder.title.setLayoutParams(params); // 변경된 파라미터를 적용
+        }
     }
 
     @Override
     public int getItemCount() {
-        return newsItems.size();
+        return 10;
     }
 
     public static class MyViewHolder extends RecyclerView.ViewHolder{
         TextView rank, title, company, date;
-        ImageView url;
+        ImageView image;
         public MyViewHolder(@NonNull View itemView) {
             super(itemView);
             title = itemView.findViewById(R.id.tvRank_title);
             company = itemView.findViewById(R.id.tvRank_company);
             date = itemView.findViewById(R.id.tvRank_date);
-            url = itemView.findViewById(R.id.iv_rank);
+            image = itemView.findViewById(R.id.iv_rank);
             rank = itemView.findViewById(R.id.tv_ranking);
         }
     }
